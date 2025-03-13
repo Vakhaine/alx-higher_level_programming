158037307a780db5508240320a7d5a265019c2e9
Vakhaine <saxonevictor@gmail.com>
2025-03-13 12:58:59 -0700

    concat edges 😊

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+new_sentence = str[39:67] + str[107:-17] + str[:7]
+str = new_sentence
+print(str)
+
